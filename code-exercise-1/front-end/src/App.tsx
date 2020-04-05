2f602493f61b2dcdc39b6c4484793528335b4f9a
stephanrotolante <stephanrotolante@gmail.com>
2020-04-05 12:41:06 -0400

    feat: updates

--- code-exercise-1/front-end/src/App.tsx
+++ code-exercise-1/front-end/src/App.tsx
@@ -25,7 +25,7 @@ const App: FunctionComponent<AppProps> =() => {
         setTypeOfValidation('text');
         setText(_value);
     }
-
+   
     const validateFile = () => {
         axios({
             method:"POST",
